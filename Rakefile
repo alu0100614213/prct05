8bac186479111b83dafd97f39ef7fec8f9b79fe7
Sebastian <alu0100614213@ull.edu.es>
2013-10-18 16:54:09 +0100

    Final de la Practica

--- Rakefile
+++ Rakefile
@@ -1,5 +1,9 @@
 desc "Tarea Por defecto"
 task :default do
-  sh "ruby racional.rb"
+	sh "ruby racional.rb"
 end
 
+desc "Ejecucion de test"
+task :test do
+	sh "ruby -Ilib -Itest tc_racional.rb"
+end
--- racional.rb
+++ racional.rb
@@ -10,11 +10,11 @@ class Fraccion
 		@b=args[1]/i 
 	end
 	
-	def to_s #funcion de pasar a string
+	def to_s #metodo string
 		"#{@a}/#{@b}"
 	end
 	
-	def suma(*args)
+	def suma(*args)#metodo suma
 		x, y = min(args[0],args[1])
 		if (@b == y) 
 			@a += x
@@ -27,7 +27,7 @@ class Fraccion
 		return @a, @b
 	end
 	
-	def resta (*args)
+	def resta (*args)#metodo resta
 		x, y = min(args[0],args[1])
 		if (@b == y) 
 			@a += x
@@ -39,30 +39,29 @@ class Fraccion
 		return @a, @b
 	end
 	
-	def producto (*args)
+	def producto (*args)#metodo producto
 		x, y = min(args[0],args[1])
 		@a, @b = min(@a * x, @b * y)
 
 		return @a, @b	
 	end
 	
-	def division (*args)
+	def division (*args)#metodo division
 		x, y = min(args[0],args[1])
 		@a, @b = min(@a / x, @b / y)
 
 		return @a, @b	
 	end
 	
-	def min(m,n)
+	def min(m,n)#metodo de reduccion 
 		i=gcd(m,n)
 		m=m/i
 		n=n/i
 		return m,n
 	end
 	
-	def gcm(u, v)
+	def gcm(u, v)#metodo minimo como un multiplo
    	u, y = u.abs, v.abs
-   
    	(u / gcd(u,v)) * v
 	end
 end
